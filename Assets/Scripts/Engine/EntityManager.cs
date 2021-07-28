e292db15064f79a1366cf41311e8450911bc5e8a
Sami DALIL <MN6195@engie.com>
2021-07-29 00:51:05 +0200

    threading unify execution of callbacks

--- Assets/Scripts/Engine/EntityManager.cs
+++ Assets/Scripts/Engine/EntityManager.cs
@@ -10,12 +10,15 @@ namespace PA.Engine
         [SerializeField] private GameObject entityPrefab = null;
 
         private readonly Dictionary<int, Entity> entities = new Dictionary<int, Entity>();
-        private readonly Stack<int> entitiesToCreate = new Stack<int>();
-        private readonly Stack<Action> actionsToLaunch = new Stack<Action>();
 
-        public void Create(int id)
+        public Entity Create(int id)
         {
-            if (!this.entities.ContainsKey(id)) this.entitiesToCreate.Push(id);
+            if (this.entities.ContainsKey(id))
+                return this.entities[id];
+
+            Entity entity = GameObject.Instantiate(this.entityPrefab).GetComponent<Entity>();
+            this.entities.Add(id, entity);
+            return entity;
         }
 
         public Entity Apply(int id, ComponentInfo component)
@@ -24,39 +27,17 @@ namespace PA.Engine
                 return null;
 
             var entity = this.entities[id];
-            try
+
+            switch ((Components)component.id)
             {
-                switch ((Components)component.id)
-                {
-                    case Components.Transform:
-                        this.actionsToLaunch.Push(() =>
-                        {
-                            entity.transform.position = component.data.position;
-                            //entity.transform.rotation = Quaternion.Euler(data.rotation);
-                            //entity.transform.localScale = data.scale;
-                        });
-                        break;
-                }
-            } catch (Exception e)
-            {
-                Debug.LogError(e.Message);
+                case Components.Transform:
+                    entity.transform.position = component.data.position;
+                    //entity.transform.rotation = Quaternion.Euler(data.rotation);
+                    //entity.transform.localScale = data.scale;
+                    break;
             }
 
             return entity;
-        }
-
-        public void Update()
-        {
-            if (this.entitiesToCreate.Count > 0)
-            {
-                int id = this.entitiesToCreate.Pop();
-                Entity entity = GameObject.Instantiate(this.entityPrefab).GetComponent<Entity>();
-                this.entities.Add(id, entity);
-            }
-            if (this.actionsToLaunch.Count > 0)
-            {
-                this.actionsToLaunch.Pop()();
-            }
         }
     }
 }
--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -1,5 +1,7 @@
 using PA.Data;
 using PA.Engine;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PA.Network
@@ -10,76 +12,69 @@ namespace PA.Network
 
         private int previousDiffFrame = 0;
         private int previousStateFrame = 0;
+        private Stack<Action> threadUnifierStack = new Stack<Action>();
         private SocketClient client = null;
-
-        private void Start()
-        {
-            this.client = new SocketClient();
-            this.client
-                .On<EntityCreatedInfo>("entityCreated", this.OnEntityCreated)
-                .On<StateUpdateInfo>("state", this.OnStateUpdate)
-                .On<StateUpdateInfo>("diff", this.OnDiffUpdate)
-                .Connect();
-        }
-
-        private void FixedUpdate()
-        {
-            if (Input.GetKey(KeyCode.W)) this.client.Emit("action", 2);
-            if (Input.GetKey(KeyCode.S)) this.client.Emit("action", 4);
-            if (Input.GetKey(KeyCode.A)) this.client.Emit("action", 1);
-            if (Input.GetKey(KeyCode.D)) this.client.Emit("action", 3);
-            if (Input.GetKey(KeyCode.Space)) this.client.Emit("action", 5);
-        }
 
         #region Events
 
         private void OnEntityCreated(EntityCreatedInfo entity)
         {
-            try
-            {
-                this.stateManager.CreateEntity(entity.id);
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e.Message);
-            }
+            this.stateManager.CreateEntity(entity.id);
         }
 
         private void OnStateUpdate(StateUpdateInfo state)
         {
-            try
+            if (this.previousStateFrame < state.frame)
             {
-                if (this.previousStateFrame < state.frame)
-                {
-                    this.previousStateFrame = state.frame;
-                    foreach (var entity in state.data)
-                        foreach (var component in entity.components)
-                        {
-                        }
-                }
-            }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e.Message);
+                this.previousStateFrame = state.frame;
+                foreach (var entity in state.data)
+                    foreach (var component in entity.components)
+                    {
+                    }
             }
         }
 
         private void OnDiffUpdate(StateUpdateInfo diff)
         {
-            try
+            if (this.previousDiffFrame < diff.frame && this.previousStateFrame < diff.frame)
             {
-                if (this.previousDiffFrame < diff.frame && this.previousStateFrame < diff.frame)
-                {
-                    this.previousDiffFrame = diff.frame;
-                    foreach (var entity in diff.data)
-                        foreach (var component in entity.components)
-                            this.stateManager.Apply(entity.id, component);
-                }
+                this.previousDiffFrame = diff.frame;
+                foreach (var entity in diff.data)
+                    foreach (var component in entity.components)
+                        this.stateManager.Apply(entity.id, component);
             }
-            catch (System.Exception e)
-            {
-                Debug.LogError(e.Message);
-            }
+        }
+
+        private Action<T> ExecuteOnMainThread<T>(Action<T> action)
+        {
+            return (arg) => this.threadUnifierStack.Push(() => action(arg));
+        }
+
+        #endregion
+
+        #region Callbacks Unity
+
+        private void Start()
+        {
+            this.client = new SocketClient();
+            this.client
+                .On("entityCreated", this.ExecuteOnMainThread<EntityCreatedInfo>(this.OnEntityCreated))
+                .On("state", this.ExecuteOnMainThread<StateUpdateInfo>(this.OnStateUpdate))
+                .On("diff", this.ExecuteOnMainThread<StateUpdateInfo>(this.OnDiffUpdate))
+                .Connect();
+        }
+
+        private void FixedUpdate()
+        {
+            while (this.threadUnifierStack.Count > 0)
+                try { this.threadUnifierStack.Pop()(); }
+                catch (Exception e) { Debug.LogError(e.Message); }
+
+            if (Input.GetKey(KeyCode.W)) this.client.Emit("action", 2);
+            if (Input.GetKey(KeyCode.S)) this.client.Emit("action", 4);
+            if (Input.GetKey(KeyCode.A)) this.client.Emit("action", 1);
+            if (Input.GetKey(KeyCode.D)) this.client.Emit("action", 3);
+            if (Input.GetKey(KeyCode.Space)) this.client.Emit("action", 5);
         }
 
         #endregion
